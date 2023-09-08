2343f7cfcccc270e8572e19a67465455f8ad8300
Alex <alex.shalimovlol@gmail.com>
2023-09-08 14:27:08 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        /*Employer e1 = new Employer("vova", "Junior", "blabbla", 79192423532l, 30000, 24 );
+        Employer e1 = new Employer("vova", "Junior", "blabbla", 79192423532l, 30000, 24 );
         Employer e2 = new Employer("Lena", "Junior", "blabbla", 79192423532l, 30000, 20 );
         Employer e3 = new Employer("Sasha", "Junior", "blabbla", 79192423532l, 30000, 41 );
         Employer e4 = new Employer("Semyon", "Junior", "blabbla", 79192423532l, 30000, 27 );
@@ -15,10 +15,10 @@ public class Main {
         emp[4] = e5;
         for (int i = 0; i < emp.length; i++) {
             if (emp[i].age > 40) {
-                System.out.print(emp[i].name);
+                System.out.print(emp[i].name + " ");
                 System.out.println(emp[i].age);
             }
-        }*/
+        }
 
 
     }
